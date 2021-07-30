b31306bf9d5d8a409eaacadf174c8a22eb88e131
MYangGu <2572280y@student.gla.ac.uk>
2021-07-30 22:40:01 +0100

    changde in5

--- rango/models.py
+++ rango/models.py
@@ -4,11 +4,12 @@ class Category(models.Model):
  name = models.CharField(max_length=128, unique=True)
  views = models.IntegerField(default=0)
  likes = models.IntegerField(default=0)
-class Meta:
+ def __str__(self):
+         return self.name
+ class Meta:
         verbose_name_plural = 'Categories'
 
-def __str__(self):
-  return self.name
+        
 
 class Page(models.Model):
       category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -16,6 +17,6 @@ class Page(models.Model):
       url = models.URLField()
       views = models.IntegerField(default=0)
 
-def __str__(self):
-  return self.titl
+      def __str__(self):
+        return self.title
 # Create your models here.
